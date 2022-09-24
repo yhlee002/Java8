137c52686d2d86977ea6476878faf9923169e69d
yhlee <xxxoxxo002@gmail.com>
2022-09-24 15:34:36 +0900

    일부 파일 경로 변경

--- src/P03_StreamAPI/App.java
+++ src/P03_StreamAPI/App.java
@@ -1,20 +1,20 @@
-package P02_Default_method_Static_method;
+package P03_StreamAPI;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class App2 {
+public class App {
     public static void main(String[] args) {
         String[] strings = new String[]{"yh", "hw", "molang"};
         List<String> names = new ArrayList<>(List.of(strings));
         List<String> limitResult = names
-                .stream().map(name -> {
+                .stream().map(name -> { // 스트림 속 반복(중개 오퍼레이터)
                     System.out.println(name); // yh
                     return name.toUpperCase();
                 })
-                .limit(1) // 1번만 실행
-                .collect(Collectors.toList()); // 리턴값을 List 타입으로 모은다.
+                .limit(1) // 1번만 실행(중개 오퍼레이터)
+                .collect(Collectors.toList()); // 리턴값을 List 타입으로 모음(종료 오퍼레이터)
 
         limitResult.forEach(System.out::println); // YH
     }
