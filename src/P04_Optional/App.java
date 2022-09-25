551da06565b70af668ae36f4e2a8db96cb94538c
yhlee <xxxoxxo002@gmail.com>
2022-09-25 21:50:35 +0900

    Optional 추가(미완)

---
+++ src/P04_Optional/App.java
@@ -0,0 +1,20 @@
+package P04_Optional;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
+        Duration studyDuration = spring_boot.getProgress().getStudyDuration();
+        System.out.println(studyDuration);
+    }
+}
