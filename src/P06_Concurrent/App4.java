cd3c02acb5ff5428469793c259d241bbd2007197
yhlee <yhlee@haebomtech.com>
2022-10-24 08:25:53 +0900

    Callable 추가 작성(invokeAll, invokeAny)

--- src/P06_Concurrent/App4.java
+++ src/P06_Concurrent/App4.java
@@ -1,22 +1,24 @@
 package P06_Concurrent;
 
+import java.util.List;
 import java.util.concurrent.*;
 
-// Callable
+// Callable, Future
 public class App4 {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     ExecutorService service = Executors.newFixedThreadPool(4);
 
-    System.out.println("Start");
+    Callable<String> collable1 = () -> "Processing1";
+    Callable<String> collable2 = () -> "Processing2";
+    Callable<String> collable3 = () -> "Processing3";
 
-    Future<String> future = service.submit(() -> "Processing"); // Collable의 T call();
-    System.out.println("isDone() " + future.isDone());
 
-    future.get();
+    List<Future<String>> futures = service.invokeAll(List.of(collable1, collable2, collable3));
+    futures.stream().forEach(f -> {
+      System.out.println(f.isDone());
+    });
 
-    System.out.println("isDone() " + future.isDone());
-    System.out.println("End!");
+    String futureAny = service.invokeAny(List.of(collable1, collable2, collable3));
+    System.out.println(futureAny);
   }
-
-
 }
